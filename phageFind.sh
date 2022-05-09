a3b3b968cf7c25f1859409cc832be4f3ea5d76d0
EleonoreL <74671135+EleonoreL@users.noreply.github.com>
2022-05-09 14:46:17 -0400

    ajout echo

--- phageFind.sh
+++ phageFind.sh
@@ -2,6 +2,7 @@
 
 
 # Co-assemblage
+echo "=>Co-assembling all reads"
 megahit -1 NOMTEMP.fq -2 NOMTEMP.fq -o NOMTEMP.out  # 1 paired-end library
 # if biodiversity in sample is very high (ex:soil) : --presets meta-large
 # TODO: sortir statistiques? (nb contigs, longueur, etc.)
@@ -10,6 +11,7 @@ megahit -1 NOMTEMP.fq -2 NOMTEMP.fq -o NOMTEMP.out  # 1 paired-end library
 
 #Trouver séquences virales phages
 #VirSorter2
+echo "=>Identifying phage sequences"
 #TODO: Vérifier que programme et db sont installés
 if(Variables){resultats}
 #Only select phages
@@ -17,3 +19,4 @@ virsorter run -w NOMTEMP.out -i NOMTEMP.fa -j 4 --include-groups "dsDNAphage,ssD
 #TODO: sortir statistiques
 
 # Trouver hôte(s)
+echo "=>Identifying phage hosts"
