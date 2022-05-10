5fbbd10a9590de09d85bc995cf1182f33d467732
EleonoreL <74671135+EleonoreL@users.noreply.github.com>
2022-05-10 14:51:14 -0400

    modifs mineures

--- phageFind.sh
+++ phageFind.sh
@@ -1,12 +1,5 @@
 # Variables pour noms/path fichiers
 
-
-# Co-assemblage
-echo "=>Co-assembling all reads"
-megahit -1 NOMTEMP.fq -2 NOMTEMP.fq -o NOMTEMP.out  # 1 paired-end library
-# if biodiversity in sample is very high (ex:soil) : --presets meta-large
-# TODO: sortir statistiques? (nb contigs, longueur, etc.)
-
 # Trouver, identifier séquences phages
 
 #Trouver séquences virales phages
