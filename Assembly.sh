6fc297cf6e05afde5e41504fd1ad14e1e61ff19c
EleonoreL <74671135+EleonoreL@users.noreply.github.com>
2022-05-12 09:59:07 -0400

    ajouté variables, créé fichier pour tests éventuels

--- Assembly.sh
+++ Assembly.sh
@@ -1,9 +1,17 @@
 # Variables pour noms/path fichiers
+name="$1"
+TBD="$2"
+TBD="$3"
+threads="$4"
 
+# TODO: DOSSIERS POUR FICHIERS
+echo "=>Creating other directories for $name"    
+mkdir "$name/3-Coassembly"
+mkdir "$name/4-Cartography"
 
 # Co-assemblage
 echo "=>Starting co-assembly of all reads"
-megahit -1 NOMTEMP.fq -2 NOMTEMP.fq -o NOMTEMP.out  # 1 paired-end library
+megahit -1 NOMTEMP.fq -2 NOMTEMP.fq -o "$name/3-Coassembly/NOMTEMP.out"  # 1 paired-end library
 # if biodiversity in sample is very high (ex:soil) : --presets meta-large
 # TODO: sortir statistiques? (nb contigs, longueur, etc.)
 
--- phageFind.sh
+++ phageFind.sh
@@ -1,23 +1,28 @@
 # Variables pour noms/path fichiers
 # Input de départ
 name="$1"
-R1="$2"
-R2="$3"
+TBD="$2"
+TBD="$3"
 threads="$4"
+cwd=$(pwd)
+
+# TODO: DOSSIERS POUR FICHIERS
+echo "=>Creating other directories for $name"    
+mkdir "$name/5-Phages"
+mkdir "$name/5-Phages/Hosts"
+mkdir "$name/5-Phages/Caracteristics"
 
 #Trouver séquences virales phages
 echo "=>Starting to predict phage sequences"
 #VirSorter2
 echo "=>Identifying phage sequences"
-#TODO: Vérifier que programme et db sont installés
-if(Variables){resultats}
 #Only select phages
-virsorter run -w NOMTEMP.out -i NOMTEMP.fa -j 4 --include-groups "dsDNAphage,ssDNA"
+virsorter run -w "$name/5-Phages/NOMTEMP.out" -i "$name/3-Coassembly/NOMTEMP.fa" -j 4 --include-groups "dsDNAphage,ssDNA"
 #TODO: sortir statistiques
 
 # Trouver hôte(s)
 echo "=>Identifying phage hosts"
 cd DeepHost_scripts
-python DeepHost.py CHANGER_Phage_genomes.fasta --out CHANGER_Output_name.txt --rank species
-
+python DeepHost.py "$name/3-Coassembly/CHANGER_Phage_genomes.fa" --out "$name/5-Phages/HostsCHANGER_Output_name.txt" --rank species
+cd cwd
 echo "=>Done with phage prediction!"
