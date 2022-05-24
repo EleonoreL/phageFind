f0708d82ee59266e91a4a5410072b9e9692e524b
EleonoreL <74671135+EleonoreL@users.noreply.github.com>
2022-05-24 09:31:54 -0400

    fait tableau résultats: size, completeness, dsdna

--- essaiAnalyse.R
+++ essaiAnalyse.R
@@ -11,24 +11,21 @@ viralScore <- read.table("final-viral-score.tsv", sep = "\t",
                          h=TRUE)
 viralCombined <- read.table("final-viral-combined.fa", h=TRUE, 
                             sep = "\t")
-completeness <- read.table("completeness.tsv", sep = "\t", h=TRUE,
-                           row.names = 1)
+completeness <- read.table("completeness.tsv", sep = "\t", h=TRUE)
 hostFile <- read.table("exemple.tsv", sep = "\t", h=TRUE,
                        row.names = 1)
 #sélectionner colonnes nécessaires dans fichiers
 size <- viralScore[,c(1,6)]
-complete <- completeness[, "TBD"]
+complete <- completeness[, 5]
 #Nom séquence et type ADN
-dna <- viralScore[, c(1,5)]
+dna <- viralScore[, 5]
 
 
 # Assembler dans un tableau
-nomsCol <- c("contigID","Size","Completeness","ssDNA_dsDNA","Taxonomy","Host","Lysogeny", "Reads_per_dataset")
-
-base <- data.frame()
-colnames(base) <- nomsCol
-row.names(base) <- row.names(viralCombined)
-
+nomsCol <- c("contigID","Size","Completeness","ssDNA_dsDNA") #,"Taxonomy","Host","Lysogeny", "Reads_per_dataset")
+combinee <- cbind(size, complete, dna)
+colnames(combinee) <- nomsCol
+#row.names(combinee) <- row.names(viralCombined)
 
 # Statistiques de bases
 
