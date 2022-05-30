10c072c294b8f7cd9aaf34028f5591260372735e
EleonoreL <74671135+EleonoreL@users.noreply.github.com>
2022-05-30 14:49:19 -0400

    modifs mineures, commentaires

--- Assembly.sh
+++ Assembly.sh
@@ -5,14 +5,12 @@ R2="$3"
 threads="$4"
 
 # TODO: DOSSIERS POUR FICHIERS
-echo "=>Creating other directories for $name"    
 mkdir "$name/3-Coassembly"
-mkdir "$name/4-Cartography"
 
 # Co-assemblage
 echo "=>Starting co-assembly of all reads"
 #TODO: VÉRIFIER SOURCE ET NOMS FICHIERS
-megahit -1 "$name/$R1".fastq -2 "$name/$R2".fastq -o "$name"/3-Coassembly/NOMTEMP.out > "$name"/3-Coassembly/Coassembly.log 2>&1 # 1 paired-end library
+megahit -1 "$name/$R1".fastq -2 "$name/$R2".fastq -o "$name/3-Coassembly/$name"_assembled.out > "$name"/3-Coassembly/Coassembly.log 2>&1 # 1 paired-end library
 # if biodiversity in sample is very high (ex:soil) : --presets meta-large
 # TODO: sortir statistiques? (nb contigs, longueur, etc.)
 
